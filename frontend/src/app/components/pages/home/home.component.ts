43e44aec1da4c4dd742e76f37531879333348c50
Aravinth Kumar <aravinth3331@gmail.com>
2023-02-25 14:39:49 +0530

    search and tag filter

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { FoodService } from 'src/app/services/food.service';
-import { Food } from '../../shared/models/food';
+import { Food } from 'src/app/shared/models/food';
+import { Tag } from 'src/app/shared/models/Tag';
 
 @Component({
   selector: 'app-home',
@@ -10,9 +12,16 @@ import { Food } from '../../shared/models/food';
 export class HomeComponent {
 
   foods: Food[] = [];
-
-  constructor(private foodService:FoodService){
+  constructor(private foodService:FoodService, activatedRoute: ActivatedRoute){
     this.foods = foodService.getAllFoods()
+    activatedRoute.params.subscribe(params=>{
+      if(params?.['searchTerm'])
+        this.foods = this.foodService.getFoodsBySearchTerm(params?.['searchTerm']);
+        else if(params?.['tag'])
+        this.foods = this.foodService.getFoodsByTag(params?.['tag']);
+        else
+        this.foods = foodService.getAllFoods();
+    })
   }
 
 }
